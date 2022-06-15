cf7005d2dcd01d8ba29f6a242672ecc04a7dccf4
Uriel Guillermo Daniel <45216219+Uriel1989@users.noreply.github.com>
2022-06-15 20:56:53 -0300

    Add files via upload

---
+++ 46-PorcentajeDeVaronesYMujeres/PorcentajeDeVaronesYMujeres/CalculoDePorcentajes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PorcentajeDeVaronesYMujeres
+{
+    class CalculoDePorcentajes
+    {
+        
+        public decimal PorcentajeDeVarones (decimal TotalDeVarones, decimal TotalAlumnos)
+        {
+            return (TotalDeVarones * 100)/TotalAlumnos;
+        }
+        public decimal PorcentajeDeMujeres(decimal TotalDeMujeres, decimal TotalAlumnos)
+        {
+            return (TotalDeMujeres * 100) / TotalAlumnos;
+        }
+    }
+}
---
+++ 46-PorcentajeDeVaronesYMujeres/PorcentajeDeVaronesYMujeres/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace PorcentajeDeVaronesYMujeres
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Un maestro desea saber que porcentaje de hombres y 
+            que porcentaje de mujeres hay en un grupo de estudiantes.*/
+
+            decimal TotalAlumnos = 0;
+
+            CalculoDePorcentajes VaronesYMujeres = new CalculoDePorcentajes();
+
+            Console.WriteLine("Ingrese el numero total de alumnos para el calculo de porcentaje:");
+
+            Console.WriteLine("Ingrese el total de alumnos varones:");
+            decimal TotalDeVarones = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el total de alumnas mujeres:");
+            decimal TotalDeMujeres = Convert.ToDecimal(Console.ReadLine());
+
+            TotalAlumnos = TotalDeMujeres + TotalDeVarones;
+
+            Console.WriteLine("El total de alumnos es de: " + " " + TotalAlumnos);
+
+            Console.WriteLine("El porcentaje de varones es de: " + " " + VaronesYMujeres.PorcentajeDeVarones(TotalAlumnos, TotalDeVarones)+"%");
+
+            Console.WriteLine("El porcentaje de mujeres es de: " + " " + VaronesYMujeres.PorcentajeDeMujeres(TotalAlumnos, TotalDeMujeres) + "%");
+
+            Console.ReadKey();
+
+        }
+    }
+}
